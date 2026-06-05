a0c89f2dad85516a86433d54ed0eb52c1958c31e
Tanishka Vishwakarma <tanishka.vishwakarma@coditas.com>
2026-06-05 20:16:59 +0530

    tailwind final setup

--- src/components/LoginComponent.js
+++ src/components/LoginComponent.js
@@ -14,16 +14,16 @@ const LoginComponent = () =>{
         <form>
             <div  className="form-field">
             <div> Username</div>
-            <div><input type="text"  id="name" value="username"/> </div>
+            <div><input type="text"  id="name" /> </div>
             </div>
             <div  className="form-field">
             <div> Password</div>
-            <div> <input type="password" id="password"  value="password" /> </div>
+            <div> <input type="password" id="password"  /> </div>
 
             </div>
     
            
-            <button className="bg-amber-400" monClick={(e)=>e.preventDefault() }>Login</button>
+            <button className="bg-amber-400" onClick={(e)=>e.preventDefault() }>Login</button>
         </form>
         </div>
        
